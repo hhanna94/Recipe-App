9697f996fc7a48f3b090c7df4921ac6779da319d
hhanna94 <heatherhanna1994@gmail.com>
2022-02-14 09:31:46 -0500

    add save recipes feature w/ firebase backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -33,7 +33,8 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+  saveRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://angular-demo-8062e-default-rtdb.firebaseio.com/recipes.json', recipes)
+      .subscribe( (res) => {
+        console.log(res);
+      })
+  }
+
+}

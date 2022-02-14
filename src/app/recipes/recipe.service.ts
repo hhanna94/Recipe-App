86817e96170f334755ef63b28f4ffbb316ad75ae
hhanna94 <heatherhanna1994@gmail.com>
2022-02-14 09:56:41 -0500

    add mapping of recipes to have ingredients always

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,22 +8,22 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
-    new Recipe("Tasty Schnitzel",
-    "A super tasy Schnitzel -- just awesome!",
-    "https://upload.wikimedia.org/wikipedia/commons/f/fc/Figlm%C3%BCller-Schnitzel_in_Wien.jpg",
-    [
-      new Ingredient("Meat", 1),
-      new Ingredient("French Fries", 20)
-    ]),
-    new Recipe("Big Fat Burger",
-    "What else do you need to say?",
-    "http://hillsdalecollegian.com/wp-content/uploads/2021/03/48574958831_a1845ea075_o.jpg",
-    [
-      new Ingredient("Buns", 2),
-      new Ingredient("Meat", 1),
-      new Ingredient("Cheese", 1),
+    // new Recipe("Tasty Schnitzel",
+    // "A super tasy Schnitzel -- just awesome!",
+    // "https://upload.wikimedia.org/wikipedia/commons/f/fc/Figlm%C3%BCller-Schnitzel_in_Wien.jpg",
+    // [
+    //   new Ingredient("Meat", 1),
+    //   new Ingredient("French Fries", 20)
+    // ]),
+    // new Recipe("Big Fat Burger",
+    // "What else do you need to say?",
+    // "http://hillsdalecollegian.com/wp-content/uploads/2021/03/48574958831_a1845ea075_o.jpg",
+    // [
+    //   new Ingredient("Buns", 2),
+    //   new Ingredient("Meat", 1),
+    //   new Ingredient("Cheese", 1),
 
-    ]),
+    // ]),
   ];
 
   getRecipes() {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map } from 'rxjs/operators'
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -18,6 +19,11 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://angular-demo-8062e-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(map (recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        })
+      }))
       .subscribe( (recipes) => {
         this.recipeService.setRecipes(recipes);
       })

d7a3339652e5bc97ce4bdf1f49b812fe3374c691
hhanna94 <heatherhanna1994@gmail.com>
2022-02-14 14:48:17 -0500

    add login feature && update error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -26,21 +27,25 @@ export class AuthComponent implements OnInit {
       return;
     }
 
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
 
     if (this.isLoginMode) {
-
+      authObs = this.authService.login(form.value.email, form.value.password)
     } else {
-      this.authService.register(form.value.email, form.value.password)
-        .subscribe( authData => {
-          console.log(authData);
-          this.isLoading = false;
-        },
-        errorMessage => {
-          this.error = errorMessage;
-          this.isLoading = false;
-        })
+      authObs = this.authService.register(form.value.email, form.value.password)
     }
+
+    authObs.subscribe( authData => {
+      console.log(authData);
+      this.isLoading = false;
+    },
+    errorMessage => {
+      this.error = errorMessage;
+      this.isLoading = false;
+    })
+
     form.reset();
   }
 
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   // A Firebase Auth ID token for the newly created user.
   idToken: string;
   // The email for the newly created user.
@@ -14,32 +14,55 @@ interface AuthResponseData {
   expiresIn: string;
   // The uid of the newly created user.
   localId: string;
+  // Whether the email is for an existing account.
+  registered?: boolean;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   register(email: string, password: string) {
     return this.http
-      .post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCLDWN9Vyvu1GvN2kk__wq6bB2cWlxquUE", {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    })
-      .pipe(catchError( errorRes => {
-        let errorMessage = "An unknown error occurred!";
-        if (!errorRes.error || !errorRes.error.error) {
-          return throwError(errorMessage);
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCLDWN9Vyvu1GvN2kk__wq6bB2cWlxquUE',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
         }
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = "This email is already in use.";
-        }
-        return throwError(errorMessage);
-      }))
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCLDWN9Vyvu1GvN2kk__wq6bB2cWlxquUE',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }
+    )
+    .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email is already in use.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Invalid email/password.';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }

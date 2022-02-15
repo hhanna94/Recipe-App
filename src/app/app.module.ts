f9928eeda61ba95136bfc6d7966fe8b6a284d248
hhanna94 <heatherhanna1994@gmail.com>
2022-02-15 11:29:37 -0500

    add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RecipesModule,
+    AuthModule,
     ShoppingListModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
   ],
   providers: [
